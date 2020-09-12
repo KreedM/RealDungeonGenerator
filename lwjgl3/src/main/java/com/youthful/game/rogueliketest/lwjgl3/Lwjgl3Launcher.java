35eeb99efae81f6db77cbf0b1e081d733c18460b
KreedM <blackknight3377@gmail.com>
2020-09-12 16:13:16 -0700

    Removed Default LWJGL3 Configuration, and its Associated Resources

--- lwjgl3/src/main/java/com/youthful/game/rogueliketest/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/youthful/game/rogueliketest/lwjgl3/Lwjgl3Launcher.java
@@ -23,12 +23,4 @@ public class Lwjgl3Launcher {
 		
 		return new Lwjgl3Application(new RoguelikeTest(), config);
 	}
-
-	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-		configuration.setTitle("RoguelikeTest");
-		configuration.setWindowedMode(640, 480);
-		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-		return configuration;
-	}
 }
